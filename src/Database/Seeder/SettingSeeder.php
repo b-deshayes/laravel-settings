8599d62fef89f84d4849525b31db5ed0c18d5dd0
Bastien Deshayes <bastien35@gmail.com>
2020-08-17 16:30:35 +0200

    🚑 Fixing auto-seeder

--- src/Database/Seeder/SettingSeeder.php
+++ src/Database/Seeder/SettingSeeder.php
@@ -15,7 +15,7 @@ class SettingSeeder extends Seeder
         collect(config('settings.defaults', []))->each(static function($value, $key) {
             $tenant = is_array($value) && !empty($value['tenant']) ? $value['tenant'] : config('settings.default_tenant', 'main');
             $value = is_array($value) && !empty($value['value']) ? $value['value'] : $value;
-            Settings::add($key, $value, [
+            Settings::add($key, (string)$value, [
                 'tenant' => $tenant
             ]);
         });
